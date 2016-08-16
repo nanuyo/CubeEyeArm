d98a4f610d40a7ae3af8f1bd83cb22ae1dd3217f
nanuyo <john@nanuyo.com>
2016-08-16 14:46:21 +0800

    Update script

--- CubeEye_install.sh
+++ CubeEye_install.sh
@@ -1,5 +1,9 @@
 sudo cp -a mtf_arm_1.1-API/lib-linux-gnueabihf/libMTF* /usr/lib
 sudo ldconfig
 sudo cp mtf_linux_1.1-API/cube-eye.rules /etc/udev/rules.d/216-cube-eye.rules
+sudo apt-get install qt4-qmake
+qmake
+make
+
 
 
